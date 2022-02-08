e1c161bdb3e279d29d6d9a6ca0b10a573f22875d
José Pedro Pereira <ze.pereira96@gmail.com>
2022-02-08 14:30:34 +0000

    done

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b)=>a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error ('OutOfBounds');
+    } else {
+    return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error ('EmptySortedList');
+    }else {
+        return Math.max(...this.items);
+      
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error ('EmptySortedList');
+    }else {
+      return Math.min(...this.items)
+  }
+}
 
-  sum() {}
+  sum() {
+    let arrSum = 0;
+    if (this.length === 0) {
+      return 0;
+    }else {
+    for(let i = 0; i < this.items.length; i++) {
+      return arrSum += this.items[i];
+  /* return this.items.reduce((acc, current)=> acc + current) */
+  }
+}
 
   avg() {}
 }
